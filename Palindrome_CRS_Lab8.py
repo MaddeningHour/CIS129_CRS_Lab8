7102f59553121a318577f6f2ff4d1691627fcdfa
MaddeningHour <CRoblesSerrano@mail.pima.edu>
2024-11-24 21:45:25 -0700

    Update Palindrome_CRS_Lab8.py
    Move all of the logic into the is_palindrome method.

--- Palindrome_CRS_Lab8.py
+++ Palindrome_CRS_Lab8.py
@@ -1,17 +1,28 @@
-#Receiving inputs from user
-myPalindrome = input("What would you like to check for a palindrome?")
+#Christopher Robles Serrano
+#Module 8 Lab 8
+#11/24/2024
+#This program check if the user submitted string is a palidrome. It ignores special characters and case sensitivity.
 
-#Removing all alphanumberic values and converting to lowercase.
-myPalindrome2 = ''.join(e for e in myPalindrome if e.isalnum()).lower()
 
-def is_palindrome(myP):
-    #Checking if our original string is equal to our reversed string. If true return true.      
-    if myP == myP[::-1]:
+def is_palindrome():
+    #Getting user's string.
+    userString = input("Enter a word to check if its a palidrome: ")
+    
+    #Converting valid characters from user input into a list of strings.
+    userString2 = [e for e in userString if e.isalnum()]
+
+    #Converting the list of strings into 1 string and into lowercase.
+    userString2 = ''.join(userString2).lower()
+
+    #Checking our formatted user string is the same if it was reversed. If true return true.
+    if userString2 == userString2[::-1]:
+        print(f'Your string "{userString}" is a palindrome.')
         return True
     
-    #If not true return false.
+    #If false return false.
     else:
+        print(f'Your string "{userString}" is not a palindrome.')
         return False
-
-#Printing our results passing in the user input as our parameter.
-print(is_palindrome(myPalindrome2))
+    
+#Calling our method via a print statement.    
+print(is_palindrome())    
